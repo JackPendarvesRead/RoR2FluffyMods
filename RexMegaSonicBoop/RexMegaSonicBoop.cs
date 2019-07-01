a7fefc1840201d0d5fb9ddb2cd4104c058849601
Jack Read <jackpendarvesread@gmail.com>
2019-07-01 01:06:09 +0100

    Add project files.

---
+++ RexMegaSonicBoop/RexMegaSonicBoop.cs
@@ -0,0 +1,66 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoR2;
+using MonoMod.Cil;
+using Mono.Cecil.Cil;
+using BepInEx.Configuration;
+
+namespace TestILHook2
+{
+
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.FluffyMods.RexMegaSonicBoop", "RexMegaSonicBoop", "1.0.1")]
+    public class RexMegaSonicBoop : BaseUnityPlugin
+    {
+        private static ConfigWrapper<float> AirKnockBackDistance;
+        private static ConfigWrapper<float> GroundKnockBackDistance;
+        private static ConfigWrapper<float> LiftVelocity;
+        private static ConfigWrapper<float> MaxDistance;
+
+        public void Awake()
+        {
+            AirKnockBackDistance = Config.Wrap(
+                                   "FireSonicBoom",
+                                   "AirKnockBackDistance",
+                                   "Set how far you knock yourself back when you boop in mid-air. (Game default = 8,  Recommended = 16)",
+                                   16f
+                                   );
+
+            GroundKnockBackDistance = Config.Wrap(
+                                   "FireSonicBoom",
+                                   "GroundKnockBackDistance", 
+                                   "Set how far you knock yourself back when you boop whilst on the ground. (Game default = 0, Recommended = 0)",
+                                   0f
+                                   );
+
+            MaxDistance = Config.Wrap(
+                                   "FireSonicBoom",
+                                   "MaxDistance",
+                                   "Set the horizontal distance which enemies should be knocked back by the boop. (Game default = 30, Recommended=100)",
+                                   100f
+                                   );
+
+            LiftVelocity = Config.Wrap(
+                                   "FireSonicBoom",
+                                   "LiftVelocity",
+                                   "Set the vertical lift of enemies affected by the boop. (Game default = 3f, Recommended = 6)",
+                                   6f
+                                   );
+
+            On.EntityStates.Treebot.Weapon.FireSonicBoom.OnEnter += FireSonicBoom_OnEnter;
+        }
+
+        private void FireSonicBoom_OnEnter(On.EntityStates.Treebot.Weapon.FireSonicBoom.orig_OnEnter orig, EntityStates.Treebot.Weapon.FireSonicBoom self)
+        {
+            self.airKnockbackDistance = AirKnockBackDistance.Value;
+            self.groundKnockbackDistance = GroundKnockBackDistance.Value;
+            self.maxDistance = MaxDistance.Value;
+            self.liftVelocity = LiftVelocity.Value;
+            orig(self);
+        }
+    }
+}
---
+++ RoR2FluffyMods/TestingStuff.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using MonoMod.Cil;
+using RoR2;
+using UnityEngine;
+
+namespace BackupEngiTurrets
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.FluffyMods.BackupEngiTurrets", "BackupEngiTurrets", "2.0.1")]
+    public class TestingStuff : BaseUnityPlugin
+    {
+        private static ConfigWrapper<bool> isTurretIncreased;
+        private static ConfigWrapper<bool> isFlameBlastIncreased;
+
+        public void Awake()
+        {
+            isTurretIncreased = Config.Wrap(
+                                   "Engineer",
+                                   "IsTurretIncreased",
+                                   "Set to true to set Place Turret (R) ability to gain stock with BackupMagazine.",
+                                   true
+                                   );
+
+            isFlameBlastIncreased = Config.Wrap(
+                                   "Mage",
+                                   "IsFlameBlastIncreased",
+                                   "Set to true to set Mage Flame Blast (LMB) ability to gain stock with BackupMagazine.",
+                                   false
+                                   );
+
+            //On.RoR2.CharacterBody.RecalculateStats += CharacterBody_RecalculateStats;
+        }
+    }
+}
