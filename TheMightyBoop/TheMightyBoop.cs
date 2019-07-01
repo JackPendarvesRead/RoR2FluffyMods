2fb393104b1409fb981b2280bd61b90c12e71fa8
Jack Read <jackpendarvesread@gmail.com>
2019-07-01 10:19:44 +0100

    Added console commands

--- TheMightyBoop/TheMightyBoop.cs
+++ TheMightyBoop/TheMightyBoop.cs
@@ -8,6 +8,7 @@ using RoR2;
 using MonoMod.Cil;
 using Mono.Cecil.Cil;
 using BepInEx.Configuration;
+using UnityEngine;
 
 namespace TheMightyBoop
 {
@@ -25,6 +26,7 @@ namespace TheMightyBoop
         {
             string sectionName = "FireSonicBoom";
 
+            #region ConfigSetup
             AirKnockBackDistance = Config.Wrap(
                                    sectionName,
                                    nameof(AirKnockBackDistance),
@@ -34,7 +36,7 @@ namespace TheMightyBoop
 
             GroundKnockBackDistance = Config.Wrap(
                                    sectionName,
-                                   nameof(GroundKnockBackDistance), 
+                                   nameof(GroundKnockBackDistance),
                                    $"Set how far you knock yourself back when you boop whilst on the ground. (Game default = {BoopConstants.GroundKnockBackDistanceDefault}, Recommended = {BoopConstants.GroundKnockBackDistanceRecommended})",
                                    BoopConstants.GroundKnockBackDistanceRecommended
                                    );
@@ -52,6 +54,7 @@ namespace TheMightyBoop
                                    $"Set the vertical lift of enemies affected by the boop. (Game default = {BoopConstants.LiftVelocityDefault}, Recommended = {BoopConstants.LiftVelocityRecommended})",
                                    BoopConstants.LiftVelocityRecommended
                                    );
+            #endregion
 
             On.EntityStates.Treebot.Weapon.FireSonicBoom.OnEnter += FireSonicBoom_OnEnter;
         }
@@ -74,5 +77,94 @@ namespace TheMightyBoop
                 orig(self);
             }
         }
+
+        #region ConsoleCommands
+
+        /// <summary>
+        /// Set default values to all configurations.
+        /// </summary>
+        [ConCommand(commandName = "boop_set_default", flags = ConVarFlags.ExecuteOnServer, helpText = "Set default values to all configurations.")]
+        private static void BoopSetDefault()
+        {
+            AirKnockBackDistance.Value = BoopConstants.AirKnockBackDistanceDefault;
+            GroundKnockBackDistance.Value = BoopConstants.GroundKnockBackDistanceDefault;
+            MaxDistance.Value = BoopConstants.MaxDistanceDefault;
+            LiftVelocity.Value = BoopConstants.LiftVelocityDefault;
+            Debug.Log("Set default values for configurations.");
+        }
+
+        /// <summary>
+        /// Set recommended values to all configurations.
+        /// </summary>
+        [ConCommand(commandName = "boop_set_recommended", flags = ConVarFlags.ExecuteOnServer, helpText = "Set recommended values to all configurations.")]
+        private static void BoopSetRecommended()
+        {
+            AirKnockBackDistance.Value = BoopConstants.AirKnockBackDistanceRecommended;
+            GroundKnockBackDistance.Value = BoopConstants.GroundKnockBackDistanceRecommended;
+            MaxDistance.Value = BoopConstants.MaxDistanceRecommended;
+            LiftVelocity.Value = BoopConstants.LiftVelocityRecommended;
+            Debug.Log("Set recommended values for configurations.");
+        }
+
+        /// <summary>
+        /// Set value for a configuration
+        /// </summary>
+        /// <param name="args">args[0]=(string)configName, args[1]=(float)value</param>
+        [ConCommand(commandName = "boop_set", flags = ConVarFlags.ExecuteOnServer, helpText = "args[0]=(string)configName, args[1]=(float)value")]
+        private static void BoopSet(ConCommandArgs args)
+        {
+            try
+            {
+                ConfigWrapper<float> wrapper;
+                switch (args[0].ToLower())
+                {
+                    default:
+                        throw new Exception("Config wrapper not found.");
+
+                    case "airknockbackdistance":
+                    case "air":
+                        wrapper = AirKnockBackDistance;
+                        break;
+
+                    case "groundknockbackdistance":
+                    case "ground":
+                        wrapper = GroundKnockBackDistance;
+                        break;
+
+                    case "liftvelocity":
+                    case "lift":
+                        wrapper = LiftVelocity;
+                        break;
+
+                    case "maxdistance":
+                    case "distance":
+                        wrapper = MaxDistance;
+                        break;
+                }
+
+                float value = float.Parse(args[1]);
+                AirKnockBackDistance.Value = value;
+                Debug.Log($"Set {args[0]}={value}");
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError(ex);
+            }
+        }
+
+        [ConCommand(commandName = "boop_get", flags = ConVarFlags.ExecuteOnServer, helpText = "Get current values to display in Chat.")]
+        private static void BoopGet()
+        {
+            Chat.AddMessage($"AirKnockback={AirKnockBackDistance.Value}\n\r" +
+                $"GroundKnockback={GroundKnockBackDistance.Value}\n\r" +
+                $"LiftVelocity={LiftVelocity.Value}\n\r" +
+                $"MaxDistance={MaxDistance.Value}\n\r");
+
+            Debug.Log($"AirKnockback ={ AirKnockBackDistance.Value}, " +
+                $"GroundKnockback={GroundKnockBackDistance.Value}, " +
+                $"LiftVelocity={LiftVelocity.Value}, " +
+                $"MaxDistance={MaxDistance.Value}");
+        }
+        #endregion
     }
 }
