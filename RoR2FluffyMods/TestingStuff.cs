a0191529e519342ec433523d61ca6250bc7f1146
Jack Read <jackpendarvesread@gmail.com>
2019-07-01 01:09:57 +0100

    Clean up

--- RoR2FluffyMods/TestingStuff.cs
+++ RoR2FluffyMods/TestingStuff.cs
@@ -7,27 +7,19 @@ using UnityEngine;
 namespace BackupEngiTurrets
 {
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.FluffyMods.BackupEngiTurrets", "BackupEngiTurrets", "2.0.1")]
+    [BepInPlugin("com.FluffyMods.TestingStuff", "TestingStuff", "0.0.0")]
     public class TestingStuff : BaseUnityPlugin
     {
-        private static ConfigWrapper<bool> isTurretIncreased;
-        private static ConfigWrapper<bool> isFlameBlastIncreased;
+        private static ConfigWrapper<bool> config;
 
         public void Awake()
         {
-            isTurretIncreased = Config.Wrap(
-                                   "Engineer",
-                                   "IsTurretIncreased",
-                                   "Set to true to set Place Turret (R) ability to gain stock with BackupMagazine.",
-                                   true
-                                   );
-
-            isFlameBlastIncreased = Config.Wrap(
-                                   "Mage",
-                                   "IsFlameBlastIncreased",
-                                   "Set to true to set Mage Flame Blast (LMB) ability to gain stock with BackupMagazine.",
-                                   false
-                                   );
+            config = Config.Wrap(                                  
+                "section",                                  
+                "key",                                 
+                "desctiption.",                                  
+                true                                   
+                );
 
             //On.RoR2.CharacterBody.RecalculateStats += CharacterBody_RecalculateStats;
         }
