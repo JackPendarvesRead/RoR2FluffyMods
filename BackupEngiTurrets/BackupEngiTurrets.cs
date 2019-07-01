b9a4ae3d95756171ea331e02459bb349637aba11
Jack Read <jackpendarvesread@gmail.com>
2019-07-01 01:07:15 +0100

    Included BackupEngiTurret in source control

---
+++ BackupEngiTurrets/BackupEngiTurrets.cs
@@ -0,0 +1,64 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using MonoMod.Cil;
+using RoR2;
+using UnityEngine;
+
+namespace BackupEngiTurrets
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.FluffyMods.BackupEngiTurrets", "BackupEngiTurrets", "2.0.1")]
+    public class BackupEngiTurrets : BaseUnityPlugin
+    {
+        private static ConfigWrapper<bool> isTurretIncreased;
+        private static ConfigWrapper<bool> isFlameBlastIncreased;
+
+        public void Awake()
+        {
+            isTurretIncreased = Config.Wrap(
+                                   "Engineer",
+                                   "IsTurretIncreased",
+                                   "Set to true to set Place Turret (R) ability to gain stock with BackupMagazine.",
+                                   true
+                                   );
+
+            isFlameBlastIncreased = Config.Wrap(
+                                   "Mage",
+                                   "IsFlameBlastIncreased",
+                                   "Set to true to set Mage Flame Blast (LMB) ability to gain stock with BackupMagazine.",
+                                   false
+                                   );
+
+            On.RoR2.CharacterBody.RecalculateStats += CharacterBody_RecalculateStats;
+        }
+
+        private void CharacterBody_RecalculateStats(On.RoR2.CharacterBody.orig_RecalculateStats orig, CharacterBody self)
+        {
+            try
+            {
+                if (self.name == CharBodyStrings.Engineer)
+                {
+                    if (isTurretIncreased.Value)
+                    {
+                        self.GetComponent<SkillLocator>().special.SetBonusStockFromBody(self.inventory.GetItemCount(ItemIndex.SecondarySkillMagazine));
+                    }
+                }
+                if(self.name == CharBodyStrings.Mage)
+                {
+                    if (isFlameBlastIncreased.Value)
+                    {
+                        self.GetComponent<SkillLocator>().primary.SetBonusStockFromBody(self.inventory.GetItemCount(ItemIndex.SecondarySkillMagazine));
+                    }
+                }
+            }
+            catch
+            {
+                Logger.LogError("CharacterBody_RecalculateStats failed.");
+            }
+            finally
+            {
+                orig(self);
+            }
+        }      
+    }
+}
---
+++ BackupEngiTurrets/CharBodyStrings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupEngiTurrets
+{
+    public static class CharBodyStrings
+    {
+        public static string Engineer { get => "EngiBody(Clone)"; }
+        public static string Mage { get => "MageBody(Clone)"; }
+    }
+}
