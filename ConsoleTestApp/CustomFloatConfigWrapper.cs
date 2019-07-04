67d9f085250e695b8ebf259b102605c6ff984ba2
Jack Read <jackpendarvesread@gmail.com>
2019-07-04 18:24:39 +0100

    Added customconfigwrapper

---
+++ ConsoleTestApp/CustomFloatConfigWrapper.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+
+public class CustomFloatConfigWrapper : BaseUnityPlugin
+{
+    private static BepInEx.Configuration.ConfigWrapper<float> ConfigWrapper;
+    private float minValue;
+    private float maxValue;
+
+    public float Value
+    {
+        get
+        {
+            if (ConfigWrapper.Value > maxValue)
+            {
+                return maxValue;
+            }
+            if (ConfigWrapper.Value < minValue)
+            {
+                return minValue;
+            }
+            return ConfigWrapper.Value;
+        }
+    }
+
+
+    public CustomFloatConfigWrapper()
+    {
+
+    }
+    public void Wrap(string sectionName, string key, string description, float defaultValue, float minValue, float maxValue)
+    {
+        ConfigWrapper = Config.Wrap(sectionName, key, description, defaultValue);
+    }
+}
