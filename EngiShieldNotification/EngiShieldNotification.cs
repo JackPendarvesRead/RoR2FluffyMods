15b9bf6eac4fadd59fb61069e204fee8e0a98dcc
Jack Read <jackpendarvesread@gmail.com>
2019-07-06 03:51:21 +0100

    Added controller

--- EngiShieldNotification/EngiShieldNotification.cs
+++ EngiShieldNotification/EngiShieldNotification.cs
@@ -20,41 +20,20 @@ namespace EngiShieldNotification
     [BepInPlugin("com.FluffyMods.EngiShieldNotification", "EngiShieldNotification", "1.0.1")]
     public class EngiShieldNotification : BaseUnityPlugin
     {
-
-        public Timer ShieldTimer { get; set; }
-        public Timer CountdownTimer { get; set; }
-        private static string DestroySoundString => "Play_engi_R_place";
-        private static GameObject OnEnterGameObject;
-        private static GameObject OnExitGameObject;
+        private static GameObject OnEnterGameObject { get; set; }
+        private static GameObject OnExitGameObject { get; set; }        
 
         public void Awake()
-        {            
-            ShieldTimer = new Timer(11 * 1000)
-            {
-                AutoReset = false,
-                Enabled = false
-            };
-            ShieldTimer.Elapsed += ShieldTimer_Elapsed;
-
-            countdownValue = 3;
-            CountdownTimer = new Timer(1 * 1000)
-            {
-                AutoReset = true,
-                Enabled = false
-            };
-            CountdownTimer.Elapsed += CountdownTimer_Elapsed;
-
+        {    
             On.EntityStates.Engi.EngiBubbleShield.Deployed.OnEnter += On_Deployed_OnEnter;
             IL.EntityStates.Engi.EngiBubbleShield.Deployed.OnEnter += IL_Deployed_OnEnter;
             On.EntityStates.Engi.EngiBubbleShield.Deployed.OnExit += On_Deployed_OnExit;
             IL.EntityStates.Engi.EngiBubbleShield.Deployed.OnExit += IL_Deployed_OnExit;            
-        }
-       
+        }       
 
         private void On_Deployed_OnEnter(On.EntityStates.Engi.EngiBubbleShield.Deployed.orig_OnEnter orig, EntityStates.Engi.EngiBubbleShield.Deployed self)
         {
-            ResetTimers();
-            ShieldTimer.Start();
+            Task.Run(() => new EngiShieldNotificationController(OnEnterGameObject).Start());
             orig(self);
         }
 
@@ -62,32 +41,6 @@ namespace EngiShieldNotification
         {
             orig(self);
         }
-
-        #region Timers
-        private void ShieldTimer_Elapsed(object sender, ElapsedEventArgs e)
-        {            
-            CountdownTimer.Start();
-        }
-
-        private int countdownValue;
-        private void CountdownTimer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            var num = Util.PlaySound(DestroySoundString, OnEnterGameObject);
-            Logger.LogInfo(DestroySoundString);
-            --countdownValue;
-            if (countdownValue <= 0)
-            {
-                ResetTimers();
-            }
-        }
-
-        private void ResetTimers()
-        {
-            CountdownTimer.Stop();
-            countdownValue = 5;
-            ShieldTimer.Stop();
-        }
-        #endregion
 
         #region ILCode
         private void IL_Deployed_OnEnter(ILContext il)
---
+++ EngiShieldNotification/EngiShieldNotificationController.cs
@@ -0,0 +1,68 @@
+﻿using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using UnityEngine;
+
+namespace EngiShieldNotification
+{
+    internal class EngiShieldNotificationController
+    {
+        private Timer ShieldTimer { get; set; }
+        private Timer CountdownTimer { get; set; }
+        private static string SoundString => "Play_engi_R_place";
+        private readonly GameObject gameObject;
+
+        public EngiShieldNotificationController(GameObject gameObject)
+        {
+            ShieldTimer = new Timer(11 * 1000)
+            {
+                AutoReset = false,
+                Enabled = false
+            };
+            ShieldTimer.Elapsed += ShieldTimer_Elapsed;
+
+            countdownValue = 3;
+            CountdownTimer = new Timer(1 * 1000)
+            {
+                AutoReset = true,
+                Enabled = false
+            };
+            CountdownTimer.Elapsed += CountdownTimer_Elapsed;
+            this.gameObject = gameObject;
+        }
+
+        public void Start()
+        {
+            ShieldTimer.Start();
+        }
+
+        private void Stop()
+        {
+            CountdownTimer.Stop();
+            countdownValue = 5;
+            ShieldTimer.Stop();
+        }
+
+        private void ShieldTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            CountdownTimer.Start();
+        }
+
+        private int countdownValue;
+        private void CountdownTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var num = Util.PlaySound(SoundString, gameObject);
+            --countdownValue;
+            if (countdownValue <= 0)
+            {
+                Stop();
+            }
+        }
+
+        
+    }
+}
