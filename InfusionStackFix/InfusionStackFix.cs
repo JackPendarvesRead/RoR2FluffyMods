989276744f91948d938c214b8d18d512418508ea
Jack Read <jackpendarvesread@gmail.com>
2019-07-27 17:12:43 +0100

    Added infusion project

---
+++ InfusionStackFix/InfusionStackFix.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using MonoMod.Cil;
+using RoR2;
+using UnityEngine;
+
+namespace InfusionStackFix
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.FluffyMods.InfusionStackFix", "InfusionStackFix", "1.0.0")]
+    public class InfusionStackFix : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            IL.RoR2.GlobalEventManager.OnCharacterDeath += GlobalEventManager_OnCharacterDeath;
+        }
+
+        private void GlobalEventManager_OnCharacterDeath(MonoMod.Cil.ILContext il)
+        {
+            var c = new ILCursor(il);
+
+        }
+    }
+}
