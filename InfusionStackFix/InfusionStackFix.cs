454341b961fa1a7d5a622c0a7a03a8c377b8066f
Jack Read <jackpendarvesread@gmail.com>
2019-07-27 20:41:01 +0100

    First version

--- InfusionStackFix/InfusionStackFix.cs
+++ InfusionStackFix/InfusionStackFix.cs
@@ -1,6 +1,10 @@
 ﻿using BepInEx;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
 using MonoMod.Cil;
+using R2API.Utils;
 using RoR2;
+using System;
 using UnityEngine;
 
 namespace InfusionStackFix
@@ -12,12 +16,45 @@ namespace InfusionStackFix
         public void Awake()
         {
             IL.RoR2.GlobalEventManager.OnCharacterDeath += GlobalEventManager_OnCharacterDeath;
+            On.RoR2.Inventory.AddInfusionBonus += Inventory_AddInfusionBonus;
+        }
+
+        private void Inventory_AddInfusionBonus(On.RoR2.Inventory.orig_AddInfusionBonus orig, Inventory self, uint value)
+        {
+            var max = self.GetItemCount(ItemIndex.Infusion) * 100;
+            if(self.infusionBonus >= max)
+            {
+                return;
+            }
+            orig(self, value);
         }
 
         private void GlobalEventManager_OnCharacterDeath(MonoMod.Cil.ILContext il)
-        {
+        {            
             var c = new ILCursor(il);
-
+            c.GotoNext(
+                x => x.MatchLdloc(51),
+                x => x.MatchLdcI4(1),
+                x => x.MatchStfld(out FieldReference fr1)
+                );
+            c.Index += 1;
+            c.Remove(); //Remove the 1
+            c.Emit(OpCodes.Ldloc, (short)29);  //Infusion Count
+            c.Emit(OpCodes.Ldloc, (short)22);  //Inventory
+            c.Emit(OpCodes.Callvirt, typeof(Inventory).GetProperty("infusionBonus").GetGetMethod());
+            c.EmitDelegate<Func<int, uint, int>>((infusionCount, bonus) =>
+            {
+                int maximumBonus = infusionCount * 100;
+                int currentBonus = (int)bonus;
+                if(maximumBonus - currentBonus > infusionCount)
+                {
+                    return infusionCount;
+                }
+                else
+                {
+                    return maximumBonus - currentBonus > 0 ? maximumBonus - currentBonus : 0;
+                }
+            });
         }
     }
 }
