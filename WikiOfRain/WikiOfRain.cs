024717e1fc70c7295d3f635454e89301bbb96acb
Jack Read <jackpendarvesread@gmail.com>
2019-10-08 22:18:40 +0100

    Bep5

--- WikiOfRain/WikiOfRain.cs
+++ WikiOfRain/WikiOfRain.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using RoR2;
 
 namespace WikiOfRain
@@ -7,18 +8,24 @@ namespace WikiOfRain
     [BepInPlugin("com.FluffyMods.WikiOfRain", "WikiOfRain", "1.0.0")]
     public class MotherfuckingFungus : BaseUnityPlugin
     {
-        //private static ConfigEntry<int> Interval;
-
+        private static ConfigEntry<int> Interval;
+        private static ConfigEntry<bool> ShouldShowWiki; 
         const string randomWikiUrl = @"https://en.wikipedia.org/wiki/Special:Random";
+        WikipediaMessageController controller;
 
-        WikipediaMessageController controller = new WikipediaMessageController(randomWikiUrl, 10);
         public void Awake()
         {
-            //Interval = Config.AddSetting<int>(
-            //    "Time",
-            //    "Time",
-            //    10,
-            //    new ConfigDescription("Time"));
+            Interval = Config.AddSetting<int>(
+                "Timer",
+                "Interval(s)",
+                10,
+                new ConfigDescription("Time(s) between each wikipedia entry being added to chat"));
+
+            ShouldShowWiki = Config.AddSetting<bool>(
+                "Wiki",
+                "ReadWiki",
+                true,
+                new ConfigDescription("Enable/Disable reading wikipedia"));
 
             On.RoR2.Run.BeginStage += Run_BeginStage;
             On.RoR2.Run.AdvanceStage += Run_AdvanceStage;
@@ -45,7 +52,7 @@ namespace WikiOfRain
         private void Run_BeginStage(On.RoR2.Run.orig_BeginStage orig, Run self)
         {
             orig(self);
-            //controller = new WikipediaMessageController(randomWikiUrl, Interval.Value);
+            controller = new WikipediaMessageController(randomWikiUrl, Interval.Value);
             controller.Start();
         }
     }
