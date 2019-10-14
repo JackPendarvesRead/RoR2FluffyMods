272c228235568ec3d7b45036a4c06a65a52a7ef0
Jack Read <jackpendarvesread@gmail.com>
2019-10-14 18:22:37 +0100

    added another test project

---
+++ FluffyLabsTest/FluffyLabsTest.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoR2;
+using MonoMod.Cil;
+using UnityEngine;
+using Mono.Cecil.Cil;
+using BepInEx.Configuration;
+
+namespace FluffyLabsTest
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.FluffyMods.FluffyLabsTest", "FluffyLabsTest", "0.0.0")]
+    public class FluffyLabsTest : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+        }
+
+       
+    }   
+}
