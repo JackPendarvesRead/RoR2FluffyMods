7a7379734fc9f1f72e3fa8029640a9476ce2338f
Jack Read <jackpendarvesread@gmail.com>
2019-10-18 16:44:50 +0100

    tests

--- FluffyLabsTest/FluffyLabsTest.cs
+++ FluffyLabsTest/FluffyLabsTest.cs
@@ -9,14 +9,35 @@ using MonoMod.Cil;
 using UnityEngine;
 using Mono.Cecil.Cil;
 using BepInEx.Configuration;
+using FluffyLabsConfigManagerTools.Util;
+using FluffyLabsConfigManagerTools.Infrastructure;
 
 namespace FluffyLabsTest
 {
     [BepInPlugin("com.FluffyMods.FluffyLabsTest", "FluffyLabsTest", "0.0.0")]
     public class FluffyLabsTest : BaseUnityPlugin
     {
+        private ConditionalConfigEntry<int> ConditionalConf;
+        private MacroConfigEntry MacroConf;
+
         public void Awake()
         {
-        }       
+            var cUtil = new ConditionalUtil(this);
+            ConditionalConf = cUtil.AddConditionalConfig<int>("con", "con", 5, true, new ConfigDescription("desc"));
+
+            var bUtil = new ButtonUtil(this);
+            bUtil.AddButtonConfig("Button", "button", "description", GetDic());
+
+            var mUtil = new MacroUtil(this);
+            MacroConf = mUtil.AddMacroConfig("Macro", "Macro", "Description", false);
+        }
+        
+        private Dictionary<string, Action> GetDic()
+        {
+            return new Dictionary<string, Action>
+            {
+                { "button", () => { Debug.Log("Button action"); } }
+            };
+        }
     }   
 }
