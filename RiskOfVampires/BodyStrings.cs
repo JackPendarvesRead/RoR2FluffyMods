1b2a6af5932a5336cd88d07a26d372b8555cda78
Jack Read <jackpendarvesread@gmail.com>
2019-10-20 01:07:46 +0100

    Started on revamp of risk of vampirism

---
+++ RiskOfVampires/BodyStrings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskOfVampirism
+{
+    internal static class BodyStrings
+    {
+        public static string Commando = "Commando";
+    }
+}
--- RiskOfVampires/RiskOfVampirism.cs
+++ RiskOfVampires/RiskOfVampirism.cs
@@ -12,51 +12,65 @@ using System.Collections.Generic;
 
 namespace RiskOfVampirism
 {
-    [BepInPlugin("com.FluffyMods.RiskOfVampirism", "RiskOfVampirism", "1.0.1")]
+    [BepInPlugin("com.FluffyMods.RiskOfVampirism", "RiskOfVampirism", "2.0.0")]
     public class RiskOfVampirism : BaseUnityPlugin
     {      
-        private static ConfigWrapper<float> Leech { get; set; }
-        private static ConfigWrapper<int> DecayTime { get; set; }
-        private static ConfigWrapper<bool> Vampire { get; set; }
+        private static ConfigEntry<float> Leech { get; set; }
+        private static ConfigEntry<int> DecayTime { get; set; }
+        private static ConfigEntry<int> DegenerationThreshold { get; set; }
+        private static ConfigEntry<bool> IsVampire { get; set; }
 
         public void Awake()
         {
-            #region ConfigWrappers
-            Leech = Config.Wrap<float>(
-                "Stats",
-                "Leech",
-                "<float> The amount leech given to vampires (% damage)",
-                0.15f);
+            #region ConfigSetup
+            const string statsSection = "Stats";
 
-            DecayTime = Config.Wrap<int>(
-                "Stats",
-                "DecayTime",
-                "<int> The time(s) for player to degenerate health to zero",
-                60);
+            Leech = Config.AddSetting<float>(
+                statsSection,
+                "%LifeLeech",
+                0.15f,
+                new ConfigDescription(
+                    "The amount leech given to vampires (% damage)",
+                    new AcceptableValueRange<float>(0, 1)));
 
-            Vampire = Config.Wrap<bool>(
+            DecayTime = Config.AddSetting<int>(
+                statsSection,
+                "HealthDecayTime",
+                60,
+                "The time(s) for player to degenerate health to zero");
+
+            DegenerationThreshold = Config.AddSetting<int>(
+                statsSection,
+                "DegenerationThreshold",
+                5,
+                "You will not degenerate below this threshold number");
+
+            IsVampire = Config.AddSetting<bool>(
                "Vampire",
                "IsAVampire",
-               "<bool> Set to true to be a vampire",
-               true);
+               true,
+               "Set to true to be a vampire");
             #endregion
-            
+
             On.RoR2.GlobalEventManager.OnHitEnemy += GlobalEventManager_OnHitEnemy;
             On.RoR2.GlobalEventManager.OnCharacterDeath += GlobalEventManager_OnCharacterDeath;
+            On.RoR2.CharacterBody.FixedUpdate += CharacterBody_FixedUpdate;
             IL.RoR2.CharacterBody.RecalculateStats += CharacterBody_RecalculateStats;
         }
 
+        // GAIN MAX HEALTH ON KILL METHOD
         private void GlobalEventManager_OnCharacterDeath(On.RoR2.GlobalEventManager.orig_OnCharacterDeath orig, GlobalEventManager self, DamageReport damageReport)
         {
             var attacker = damageReport.damageInfo.attacker.GetComponent<CharacterBody>();
             var player = GetPlayer(attacker);
-            if (player != null && Vampire.Value)
+            if (player != null && IsVampire.Value)
             {
                 attacker.baseMaxHealth += 1;
             }
             orig(self, damageReport);
         }        
 
+        // LIFESTEAL METHOD
         private void GlobalEventManager_OnHitEnemy(On.RoR2.GlobalEventManager.orig_OnHitEnemy orig, GlobalEventManager self, DamageInfo damageInfo, GameObject victim)
         {
             if (!damageInfo.procChainMask.HasProc(ProcType.HealOnHit))
@@ -65,14 +79,14 @@ namespace RiskOfVampirism
                 var healthComponent = attacker.GetComponent<HealthComponent>();
                 var player = GetPlayer(attacker);
                 if (player != null 
-                    && Vampire.Value
+                    && IsVampire.Value
                     && (bool)((UnityEngine.Object)healthComponent))
                 {                    
                     var procChainMask = damageInfo.procChainMask;
                     procChainMask.AddProc(ProcType.HealOnHit);
 
-                    //var survivorCoefficient = GetSurvivorCoefficient(attacker); 
-                    //var num = (double)healthComponent.Heal((5 * damageInfo.procCoefficient + attacker.level / 2) * survivorCoefficient, procChainMask, true); 
+                    // var survivorCoefficient = GetSurvivorCoefficient(attacker); 
+                    // var num = (double)healthComponent.Heal((5 * damageInfo.procCoefficient + attacker.level / 2) * survivorCoefficient, procChainMask, true); 
                     
                     var num = (double)healthComponent.Heal(damageInfo.damage * Leech.Value, procChainMask, true);                    
                 }                             
@@ -80,26 +94,21 @@ namespace RiskOfVampirism
             orig(self, damageInfo, victim);
         }
         
+        // SET REGEN (HEALTH DECAY) METHOD
         private void CharacterBody_RecalculateStats(ILContext il)
         {
             var c = new ILCursor(il);
-            c.GotoNext(
-                x => x.MatchLdarg(0),
-                x => x.MatchLdfld(out FieldReference fr1),
-                x => x.MatchLdarg(0),
-                x => x.MatchLdfld(out FieldReference fr2),
-                x => x.MatchLdloc(27),
-                x => x.MatchMul(),
-                x => x.MatchAdd(),
-                x => x.MatchStloc(36)
-                );
-            c.Index -= 1;
+            c.GotoNext(x => x.MatchCallvirt<CharacterBody>("set_regen"));
             c.Emit(OpCodes.Ldarg_0);
             c.EmitDelegate<Func<float, CharacterBody, float>>((a, body) =>
             {
                 var player = GetPlayer(body);
-                if(player != null && Vampire.Value)
+                if(player != null && IsVampire.Value)
                 {
+                    if (body.healthComponent.health <= DegenerationThreshold.Value)
+                    {
+                        return 0;
+                    }
                     return -1f * body.maxHealth / DecayTime.Value * (body.inventory.GetItemCount(ItemIndex.LunarDagger) + 1);
                 }
                 else
@@ -107,100 +116,57 @@ namespace RiskOfVampirism
                     return a;
                 }                
             });
-        }    
+        }
 
-        private float GetSurvivorCoefficient(CharacterBody body)
+        // Threshold logic for degeneration
+        bool degenerating = true;
+        private void CharacterBody_FixedUpdate(On.RoR2.CharacterBody.orig_FixedUpdate orig, CharacterBody self)
         {
-            if (body.name.ToLower().StartsWith("commando")
-                || body.name.ToLower().StartsWith("multi"))
+            orig(self);
+            var player = GetPlayer(self);
+            if (player != null)
             {
-                return 0.5f;
+                var health = self.healthComponent.health;
+                if (degenerating
+                    && self.healthComponent.health <= DegenerationThreshold.Value)
+                {
+                    degenerating = false;
+                    self.RecalculateStats();
+                }
+                if (!degenerating
+                    && self.healthComponent.health > DegenerationThreshold.Value)
+                {
+                    degenerating = true;
+                    self.RecalculateStats();
+                }
             }
-            if (body.name.ToLower().StartsWith("engineer")
-                || body.name.ToLower().StartsWith("mage")
-                || body.name.ToLower().StartsWith("treebot"))
-            {
-                return 3f;
-            }
-            return 1f;
         }
 
         private NetworkUser GetPlayer(CharacterBody body)
         {
-            return NetworkUser.readOnlyInstancesList.Where(nu => nu.GetCurrentBody() == body).FirstOrDefault();
+            return NetworkUser.readOnlyInstancesList
+                .Where(nu => nu.GetCurrentBody() == body)
+                .FirstOrDefault();
         }
 
-        #region ConsoleCommands
-        /// <summary>
-        /// Sets the number of leech items given at start of run.
-        /// </summary>
-        /// <param name="args">args\[0\]=Value(int)</param>
-        [ConCommand(commandName = "vampire_leech", flags = ConVarFlags.ExecuteOnServer, helpText = "Sets the number of leech items given at start of run. args[0]=Value(int).")]
-        private static void VampireLeechSet(ConCommandArgs args)
-        {
-            try
-            {
-                Leech.Value = float.Parse(args[0]);
-                Debug.Log($"Leech set to {args[0]}");
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError(ex);
-            }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="args">args[0]=bool</param>
-        [ConCommand(commandName = "vampire_role", flags = ConVarFlags.ExecuteOnServer, helpText = "Sets the number of leech items given at start of run. args[0]=Value(int).")]
-        private static void VampireRole(ConCommandArgs args)
-        {
-            try
-            {
-                switch (args[0])
-                {
-                    case "1":
-                    case "true":
-                    case "t":
-                        Vampire.Value = true;
-                        Debug.Log("You are now a vampire");
-                        break;
-
-                    case "0":
-                    case "false":
-                    case "f":
-                        Vampire.Value = false;
-                        Debug.Log("You are no longer a vampire");
-                        break;
-
-                    default:
-                        throw new Exception("Command arguements not valid.");
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError(ex);
-            }
-        }
-
-        /// <summary>
-        /// Sets the decay time given at start of run.
-        /// </summary>
-        /// <param name="args">args\[0\]=Value(int)</param>
-        [ConCommand(commandName = "vampire_decay", flags = ConVarFlags.ExecuteOnServer, helpText = "Sets decay time given at start of run. args[0]=Value(int).")]
-        private static void VampireDecaySet(ConCommandArgs args)
-        {
-            try
-            {
-                DecayTime.Value = Int32.Parse(args[0]);
-                Debug.Log($"decay time set to {args[0]}");
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError(ex);
-            }
-        }
-        #endregion
+        //private float GetSurvivorCoefficient(CharacterBody body)
+        //{
+        //    Debug.Log($"VAMPIRE BODY = {body.name}");
+        //    if (body.name.ToLower().StartsWith(SurvivorIndex.Commando.ToString().ToLower())
+        //        || body.name.ToLower().StartsWith(SurvivorIndex.Toolbot.ToString().ToLower()))
+        //    {
+        //        Debug.Log("RETURN 0.5f");
+        //        return 0.5f;
+        //    }
+        //    if (body.name.ToLower().StartsWith(SurvivorIndex.Engi.ToString().ToLower())
+        //        || body.name.ToLower().StartsWith(SurvivorIndex.Mage.ToString().ToLower())
+        //        || body.name.ToLower().StartsWith(SurvivorIndex.Treebot.ToString().ToLower()))
+        //    {
+        //        Debug.Log("RETURN 3f");
+        //        return 3f;
+        //    }
+        //    Debug.Log("RETURN 1f");
+        //    return 1f;
+        //}
     }
 }
