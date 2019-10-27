afc4ade4885cb7547b92368a2001d11cde03e519
Jack Read <jackpendarvesread@gmail.com>
2019-10-27 18:21:51 +0000

    Fixed references

--- RoR2FluffyMods/CommandHelper.cs
+++ RoR2FluffyMods/CommandHelper.cs
@@ -1,36 +1,35 @@
-﻿using R2API.Utils;
-using RoR2;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using RoR2;
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Reflection;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace RoR2FluffyMods
-{
-    internal class CommandHelper
-    {
-        public static void RegisterCommands(RoR2.Console self)
-        {
-            var types = typeof(CommandHelper).Assembly.GetTypes();
-            var catalog = self.GetFieldValue<IDictionary>("concommandCatalog");
+//namespace RoR2FluffyMods
+//{
+//    internal class CommandHelper
+//    {
+//        public static void RegisterCommands(RoR2.Console self)
+//        {
+//            var types = typeof(CommandHelper).Assembly.GetTypes();
+//            var catalog = self.GetFieldValue<IDictionary>("concommandCatalog");
 
-            foreach (var methodInfo in types.SelectMany(x => x.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)))
-            {
-                var customAttributes = methodInfo.GetCustomAttributes(false);
-                foreach (var attribute in customAttributes.OfType<ConCommandAttribute>())
-                {
-                    var conCommand = Reflection.GetNestedType<RoR2.Console>("ConCommand").Instantiate();
+//            foreach (var methodInfo in types.SelectMany(x => x.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)))
+//            {
+//                var customAttributes = methodInfo.GetCustomAttributes(false);
+//                foreach (var attribute in customAttributes.OfType<ConCommandAttribute>())
+//                {
+//                    var conCommand = Reflection.GetNestedType<RoR2.Console>("ConCommand").Instantiate();
 
-                    conCommand.SetFieldValue("flags", attribute.flags);
-                    conCommand.SetFieldValue("helpText", attribute.helpText);
-                    conCommand.SetFieldValue("action", (RoR2.Console.ConCommandDelegate)Delegate.CreateDelegate(typeof(RoR2.Console.ConCommandDelegate), methodInfo));
+//                    conCommand.SetFieldValue("flags", attribute.flags);
+//                    conCommand.SetFieldValue("helpText", attribute.helpText);
+//                    conCommand.SetFieldValue("action", (RoR2.Console.ConCommandDelegate)Delegate.CreateDelegate(typeof(RoR2.Console.ConCommandDelegate), methodInfo));
 
-                    catalog[attribute.commandName.ToLower()] = conCommand;
-                }
-            }
-        }
-    }
-}
+//                    catalog[attribute.commandName.ToLower()] = conCommand;
+//                }
+//            }
+//        }
+//    }
+//}
