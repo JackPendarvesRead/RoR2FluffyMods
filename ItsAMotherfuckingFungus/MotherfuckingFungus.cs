d640953c8b6a9fd61ef97f741022414c18c54d13
Jack Read <jackpendarvesread@gmail.com>
2019-11-11 19:35:08 +0000

    tidy

--- ItsAMotherfuckingFungus/MotherfuckingFungus.cs
+++ ItsAMotherfuckingFungus/MotherfuckingFungus.cs
@@ -1,11 +1,6 @@
 ﻿using BepInEx;
-using BepInEx.Configuration;
-using MonoMod.Cil;
 using RoR2;
 using UnityEngine;
-using Mono.Cecil;
-using Mono.Cecil.Cil;
-using System;
 using System.Linq;
 
 namespace MotherfuckingFungus
@@ -21,8 +16,6 @@ namespace MotherfuckingFungus
 
         public void Awake()
         {
-            //RoR2.SceneDirector.onPostPopulateSceneServer += SceneDirector_onPostPopulateSceneServer;
-
             On.RoR2.Stage.Start += Stage_Start;
             On.RoR2.PickupDropletController.CreatePickupDroplet += SendMessageOnFungusDrop;
             On.RoR2.GenericPickupController.SendPickupMessage += SendMessageOnFungusPickup;
@@ -31,24 +24,6 @@ namespace MotherfuckingFungus
         private void Stage_Start(On.RoR2.Stage.orig_Start orig, Stage self)
         {
             orig(self);
-            if (RoR2.Run.instance
-               && NetworkUser.readOnlyInstancesList
-               .Select(x => x.GetCurrentBody())
-               .Where(x => x.bodyIndex == SurvivorCatalog.GetBodyIndexFromSurvivorIndex(SurvivorIndex.Engi))
-               .Any())
-            {
-                EngineerInGame = true;
-                Message.SendToAll("Give yo' motherfucking fungus to the motherfucking Engineer motherfuckers", Colours.LightBlue);
-            }
-            else
-            {
-                EngineerInGame = false;
-                Logger.LogInfo("No Engineer in party.");
-            }
-        }
-
-        private void SceneDirector_onPostPopulateSceneServer(SceneDirector obj)
-        {
             if (RoR2.Run.instance
                && NetworkUser.readOnlyInstancesList
                .Select(x => x.GetCurrentBody())
