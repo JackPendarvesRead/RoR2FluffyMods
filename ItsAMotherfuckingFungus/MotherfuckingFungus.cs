d97fc546aed9022dadea71352e85ff5a383182ff
Jack Read <jackpendarvesread@gmail.com>
2019-11-11 19:07:31 +0000

    Updated motherfucker

--- ItsAMotherfuckingFungus/MotherfuckingFungus.cs
+++ ItsAMotherfuckingFungus/MotherfuckingFungus.cs
@@ -21,19 +21,39 @@ namespace MotherfuckingFungus
 
         public void Awake()
         {
-            RoR2.SceneDirector.onPostPopulateSceneServer += SceneDirector_onPostPopulateSceneServer;
+            //RoR2.SceneDirector.onPostPopulateSceneServer += SceneDirector_onPostPopulateSceneServer;
 
+            On.RoR2.Stage.Start += Stage_Start;
             On.RoR2.PickupDropletController.CreatePickupDroplet += SendMessageOnFungusDrop;
             On.RoR2.GenericPickupController.SendPickupMessage += SendMessageOnFungusPickup;
+        }
+
+        private void Stage_Start(On.RoR2.Stage.orig_Start orig, Stage self)
+        {
+            orig(self);
+            if (RoR2.Run.instance
+               && NetworkUser.readOnlyInstancesList
+               .Select(x => x.GetCurrentBody())
+               .Where(x => x.bodyIndex == SurvivorCatalog.GetBodyIndexFromSurvivorIndex(SurvivorIndex.Engi))
+               .Any())
+            {
+                EngineerInGame = true;
+                Message.SendToAll("Give yo' motherfucking fungus to the motherfucking Engineer motherfuckers", Colours.LightBlue);
+            }
+            else
+            {
+                EngineerInGame = false;
+                Logger.LogInfo("No Engineer in party.");
+            }
         }
 
         private void SceneDirector_onPostPopulateSceneServer(SceneDirector obj)
         {
             if (RoR2.Run.instance
-                && NetworkUser.readOnlyInstancesList
-               .Where(u => u.GetCurrentBody().name.StartsWith(CharBodyStrings.Engineer))
-               .FirstOrDefault()
-               != null)
+               && NetworkUser.readOnlyInstancesList
+               .Select(x => x.GetCurrentBody())
+               .Where(x => x.bodyIndex == SurvivorCatalog.GetBodyIndexFromSurvivorIndex(SurvivorIndex.Engi))
+               .Any())
             {
                 EngineerInGame = true;
                 Message.SendToAll("Give yo' motherfucking fungus to the motherfucking Engineer motherfuckers", Colours.LightBlue);
@@ -60,7 +80,7 @@ namespace MotherfuckingFungus
             if (EngineerInGame 
                 && RoR2.PickupCatalog.GetPickupDef(pickupIndex).itemIndex == ItemIndex.Mushroom)
             {
-                if (master.GetBody().name.StartsWith(CharBodyStrings.Engineer))
+                if (master.GetBody().bodyIndex == SurvivorCatalog.GetBodyIndexFromSurvivorIndex(SurvivorIndex.Engi))
                 {
                     Message.SendToAll("Mmmm! That is a tasty fungus!", Colours.Green);
                 }
