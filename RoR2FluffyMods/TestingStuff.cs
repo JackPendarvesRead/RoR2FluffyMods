01857ba17777e77c62aa6fb304d257b97fe062ad
Jack Read <jackpendarvesread@gmail.com>
2019-11-14 23:01:57 +0000

    blank test

--- RoR2FluffyMods/TestingStuff.cs
+++ RoR2FluffyMods/TestingStuff.cs
@@ -16,39 +16,8 @@ namespace RoR2FluffyMods
         private const string pluginName = "InputLogger";
         private const string pluginVersion = "1.0.0";
 
-        private ConfigEntry<bool> EnableLogging;
-        private ConfigEntry<LogLevel> LoggingLevel;
-
         public void Awake()
         {
-            EnableLogging = Config.Bind<bool>("Enable/Disable", "Enable logging", true, "Enable/Disable input logging");
-            LoggingLevel = Config.Bind<LogLevel>("Enable/Disable", "Enable logging", LogLevel.Info, "Enable/Disable input logging");
-        }
-
-        public void Update()
-        {
-            var ray = new Ray(new Vector3(0,0,0), new Vector3(0,0,0));
-            
-            
-            
-
-
-            RoR2.NetworkUser.readOnlyLocalPlayersList[0].GetCurrentBody();
-
-            if (EnableLogging.Value)
-            {
-                foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
-                {
-                    if (Input.GetKeyDown(key))
-                    {
-                        Logger.Log(LoggingLevel.Value, $"InputLogger: {key.ToString()} down");
-                    }
-                    if (Input.GetKeyUp(key))
-                    {
-                        Logger.Log(LoggingLevel.Value, $"InputLogger: {key.ToString()} up");
-                    }
-                }
-            }            
-        }        
+        }       
     }
 }
