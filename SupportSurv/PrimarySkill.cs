e299d420efa1031730aa73353080f633d2e13832
Jack Read <jackpendarvesread@gmail.com>
2019-11-19 00:13:42 +0000

    playing with vectors

--- SupportSurv/PrimarySkill.cs
+++ SupportSurv/PrimarySkill.cs
@@ -1,59 +1,46 @@
 ﻿using EntityStates;
 using RoR2;
 using RoR2.Projectile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace EntityStates
 {
-    public class PrimarySkill : EntityStates.Treebot.Weapon.FireSonicBoom
+    public class PrimarySkill : BaseState
     {
-        private Ray aimRay;
-        private Transform modelTransform;
-        private string targetMuzzle;
-        private GameObject projectilePrefab;
-        private float spreadBloomValue;
-        private float damageCoefficient;
+        private readonly GameObject prefab = (GameObject)Resources.Load(@"prefabs/effects/tracers/tracersmokeline/tracermagelightninglaser");
 
         public override void OnEnter()
         {
-            this.idealDistanceToPlaceTargets = 5;
             base.OnEnter();
-            //this.aimRay = this.GetAimRay();
-            //if ((bool)((Object)this.modelTransform))
-            //{
-            //    ChildLocator component = this.modelTransform.GetComponent<ChildLocator>();
-            //    if ((bool)((Object)component))
-            //    {
-            //        Transform child = component.FindChild(targetMuzzle);
-            //        if ((bool)((Object)child))
-            //            this.aimRay.origin = child.position;
-            //    }
-            //}
-            //if (this.isAuthority)
-            //{
-            //    Vector3 forward = Vector3.ProjectOnPlane(this.aimRay.direction, Vector3.up);
-            //    var info = new FireProjectileInfo
-            //    {
-            //        projectilePrefab = projectilePrefab,
-            //        crit = false,
-            //        damage = 1,
-            //        damageColorIndex = DamageColorIndex.Default,
-            //        force = 1,
-            //        owner = gameObject,
-            //        position = transform.position,
-            //        procChainMask = new ProcChainMask(),
-            //        rotation = Util.QuaternionSafeLookRotation(forward),
-            //        target = null,
-            //        useFuseOverride = false,
-            //        useSpeedOverride = false
-            //    };
-            //    ProjectileManager.instance.FireProjectile(info);
-            //}
-            //this.characterBody.AddSpreadBloom(spreadBloomValue);
+            Debug.Log("Primary Enter");
+            var aimRay = this.GetAimRay();
+            var attack = new BulletAttack
+            {
+                aimVector = aimRay.direction,
+                origin = aimRay.origin,
+                damage = 50,
+                bulletCount = 1,
+                damageColorIndex = DamageColorIndex.Default,
+                damageType = DamageType.Generic,
+                falloffModel = BulletAttack.FalloffModel.None,
+                force = 0,
+                isCrit = false,
+                HitEffectNormal = true,
+                maxDistance = 100,
+                maxSpread = 5,
+                minSpread = 3,
+                tracerEffectPrefab = prefab
+            };
+            attack.Fire();            
         }
 
         public override void OnExit()
         {
+            Debug.Log("Primary Exit");
             base.OnExit();
         }
 
